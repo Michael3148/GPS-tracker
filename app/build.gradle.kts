03bfd9a8ddd669afb82a0ada708a38855c0008e6
Michael teferi <michaelteferi97@gmail.com>
2026-09-08 10:16:56 +0300

    Replaced the google map by osmdroid map layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
     implementation("androidx.work:work-runtime:2.9.1")
 }
